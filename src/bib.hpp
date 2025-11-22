384c8752c31460968f5a76c1bdf6a82d6d840e6b
Juliana Aparecida Borges <107365569+julianaapborges@users.noreply.github.com>
2025-11-22 10:49:22 -0300

    Delete src directory

--- src/bib.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef BIB_HPP
-#define BIB_HPP
-
-long long calcularFatorial(int n);
-
-#endif // BIB_HPP
--- src/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "model.hpp"
-#include "system.hpp"
-#include "flow.hpp"
-
-int main() {
-    return 0;
-}
